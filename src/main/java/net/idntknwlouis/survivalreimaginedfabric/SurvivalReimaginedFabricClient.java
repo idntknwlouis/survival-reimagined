aaca8e8e7e2e0959df838d593c5cd2e94b955301
louis :) <louis.tamayo1811+github@gmail.com>
2026-01-19 03:17:05 +0800

    Initial commit

---
+++ src/main/java/net/idntknwlouis/survivalreimaginedfabric/SurvivalReimaginedFabricClient.java
@@ -0,0 +1,10 @@
+package net.idntknwlouis.survivalreimaginedfabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SurvivalReimaginedFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
