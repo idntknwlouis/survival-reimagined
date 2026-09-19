7618cac14847e1add12750e66ba392c7a723ce4b
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 14:09:55 +0800

    Port FallingWisteriaParticle.java to Fabric

---
+++ src/main/java/net/mcreator/survivalreimagined/client/particle/FallingWisteriaParticle.java
@@ -0,0 +1,67 @@
+package net.mcreator.survivalreimagined.client.particle;
+
+
+import net.minecraft.world.level.Level;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.multiplayer.ClientLevel;
+
+import net.mcreator.survivalreimagined.procedures.FallingWisteriaAdditionalParticleExpiryConditionProcedure;
+
+public class FallingWisteriaParticle extends TextureSheetParticle {
+	public static FallingWisteriaParticleProvider provider(SpriteSet spriteSet) {
+		return new FallingWisteriaParticleProvider(spriteSet);
+	}
+
+	public static class FallingWisteriaParticleProvider implements ParticleProvider<SimpleParticleType> {
+		private final SpriteSet spriteSet;
+
+		public FallingWisteriaParticleProvider(SpriteSet spriteSet) {
+			this.spriteSet = spriteSet;
+		}
+
+		public Particle createParticle(SimpleParticleType typeIn, ClientLevel worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+			return new FallingWisteriaParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, this.spriteSet);
+		}
+	}
+
+	private final SpriteSet spriteSet;
+	private float angularVelocity;
+	private float angularAcceleration;
+
+	protected FallingWisteriaParticle(ClientLevel world, double x, double y, double z, double vx, double vy, double vz, SpriteSet spriteSet) {
+		super(world, x, y, z);
+		this.spriteSet = spriteSet;
+		this.setSize(0.2f, 0.2f);
+		this.quadSize *= 0.3f;
+		this.lifetime = 300;
+		this.gravity = 0.1f;
+		this.hasPhysics = true;
+		this.xd = vx * 1;
+		this.yd = vy * 1;
+		this.zd = vz * 1;
+		this.angularVelocity = 0.025f;
+		this.angularAcceleration = 0f;
+		this.pickSprite(spriteSet);
+	}
+
+	@Override
+	public ParticleRenderType getRenderType() {
+		return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		this.oRoll = this.roll;
+		this.roll += this.angularVelocity;
+		this.angularVelocity += this.angularAcceleration;
+		Level world = this.level;
+		if (FallingWisteriaAdditionalParticleExpiryConditionProcedure.execute(onGround))
+			this.remove();
+	}
+}
