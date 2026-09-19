d300cf2ca6dcd9c3721b878bdd861a4e841b06d5
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 14:09:58 +0800

    Port RadiatedParticle.java to Fabric

---
+++ src/main/java/net/mcreator/survivalreimagined/client/particle/RadiatedParticle.java
@@ -0,0 +1,66 @@
+package net.mcreator.survivalreimagined.client.particle;
+
+
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.multiplayer.ClientLevel;
+
+public class RadiatedParticle extends TextureSheetParticle {
+	public static RadiatedParticleProvider provider(SpriteSet spriteSet) {
+		return new RadiatedParticleProvider(spriteSet);
+	}
+
+	public static class RadiatedParticleProvider implements ParticleProvider<SimpleParticleType> {
+		private final SpriteSet spriteSet;
+
+		public RadiatedParticleProvider(SpriteSet spriteSet) {
+			this.spriteSet = spriteSet;
+		}
+
+		public Particle createParticle(SimpleParticleType typeIn, ClientLevel worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+			return new RadiatedParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, this.spriteSet);
+		}
+	}
+
+	private final SpriteSet spriteSet;
+	private float angularVelocity;
+	private float angularAcceleration;
+
+	protected RadiatedParticle(ClientLevel world, double x, double y, double z, double vx, double vy, double vz, SpriteSet spriteSet) {
+		super(world, x, y, z);
+		this.spriteSet = spriteSet;
+		this.setSize(0.2f, 0.2f);
+		this.quadSize *= 0.5f;
+		this.lifetime = 100;
+		this.gravity = 0f;
+		this.hasPhysics = true;
+		this.xd = vx * 0.2;
+		this.yd = vy * 0.2;
+		this.zd = vz * 0.2;
+		this.angularVelocity = 0.05f;
+		this.angularAcceleration = 0f;
+		this.pickSprite(spriteSet);
+	}
+
+	@Override
+	public int getLightColor(float partialTick) {
+		return 15728880;
+	}
+
+	@Override
+	public ParticleRenderType getRenderType() {
+		return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		this.oRoll = this.roll;
+		this.roll += this.angularVelocity;
+		this.angularVelocity += this.angularAcceleration;
+	}
+}
