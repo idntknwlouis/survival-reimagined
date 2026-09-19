671d05239af039395ac0bc3d87c3ec5e5bf0250c
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 13:46:02 +0800

    Add Fabric datagen initializer

---
+++ src/main/java/net/mcreator/survivalreimagined/SurvivalReimaginedModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mcreator.survivalreimagined;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SurvivalReimaginedModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		// Existing data-pack resources are copied from the NeoForge project.
+	}
+}
