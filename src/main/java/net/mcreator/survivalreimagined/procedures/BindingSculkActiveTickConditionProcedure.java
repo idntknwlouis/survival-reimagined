743ea63a06ebdbeff3b514a1cd94bb035f9c8afe
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 14:22:54 +0800

    Port BindingSculkActiveTickConditionProcedure.java to Fabric

---
+++ src/main/java/net/mcreator/survivalreimagined/procedures/BindingSculkActiveTickConditionProcedure.java
@@ -0,0 +1,11 @@
+package net.mcreator.survivalreimagined.procedures;
+
+public class BindingSculkActiveTickConditionProcedure {
+	public static boolean execute(double amplifier, double duration) {
+		double rateWithAmplifier = 50 / Math.pow(2, amplifier);
+		if (Math.floor(rateWithAmplifier) > 0) {
+			return duration % Math.floor(rateWithAmplifier) == 0;
+		}
+		return true;
+	}
+}
