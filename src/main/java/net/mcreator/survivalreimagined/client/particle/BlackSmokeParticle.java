9447b73191d7c3f40ac231a6a082d5a299142a1e
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 14:09:50 +0800

    Port BlackSmokeParticle.java to Fabric

---
+++ src/main/java/net/mcreator/survivalreimagined/client/particle/BlackSmokeParticle.java
@@ -0,0 +1,53 @@
+package net.mcreator.survivalreimagined.client.particle;
+
+
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.multiplayer.ClientLevel;
+
+public class BlackSmokeParticle extends TextureSheetParticle {
+	public static BlackSmokeParticleProvider provider(SpriteSet spriteSet) {
+		return new BlackSmokeParticleProvider(spriteSet);
+	}
+
+	public static class BlackSmokeParticleProvider implements ParticleProvider<SimpleParticleType> {
+		private final SpriteSet spriteSet;
+
+		public BlackSmokeParticleProvider(SpriteSet spriteSet) {
+			this.spriteSet = spriteSet;
+		}
+
+		public Particle createParticle(SimpleParticleType typeIn, ClientLevel worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+			return new BlackSmokeParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, this.spriteSet);
+		}
+	}
+
+	private final SpriteSet spriteSet;
+
+	protected BlackSmokeParticle(ClientLevel world, double x, double y, double z, double vx, double vy, double vz, SpriteSet spriteSet) {
+		super(world, x, y, z);
+		this.spriteSet = spriteSet;
+		this.setSize(0.2f, 0.2f);
+		this.lifetime = 60;
+		this.gravity = 0f;
+		this.hasPhysics = false;
+		this.xd = vx * 1;
+		this.yd = vy * 1;
+		this.zd = vz * 1;
+		this.pickSprite(spriteSet);
+	}
+
+	@Override
+	public ParticleRenderType getRenderType() {
+		return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+	}
+}
