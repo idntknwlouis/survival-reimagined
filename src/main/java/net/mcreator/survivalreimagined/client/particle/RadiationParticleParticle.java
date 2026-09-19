8d6d3ff71642c99eae4b3fbac7405260d598f56c
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 14:10:03 +0800

    Port RadiationParticleParticle.java to Fabric

---
+++ src/main/java/net/mcreator/survivalreimagined/client/particle/RadiationParticleParticle.java
@@ -0,0 +1,61 @@
+package net.mcreator.survivalreimagined.client.particle;
+
+
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraft.client.particle.TextureSheetParticle;
+import net.minecraft.client.particle.SpriteSet;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.particle.ParticleProvider;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.multiplayer.ClientLevel;
+
+public class RadiationParticleParticle extends TextureSheetParticle {
+	public static RadiationParticleParticleProvider provider(SpriteSet spriteSet) {
+		return new RadiationParticleParticleProvider(spriteSet);
+	}
+
+	public static class RadiationParticleParticleProvider implements ParticleProvider<SimpleParticleType> {
+		private final SpriteSet spriteSet;
+
+		public RadiationParticleParticleProvider(SpriteSet spriteSet) {
+			this.spriteSet = spriteSet;
+		}
+
+		public Particle createParticle(SimpleParticleType typeIn, ClientLevel worldIn, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
+			return new RadiationParticleParticle(worldIn, x, y, z, xSpeed, ySpeed, zSpeed, this.spriteSet);
+		}
+	}
+
+	private final SpriteSet spriteSet;
+
+	protected RadiationParticleParticle(ClientLevel world, double x, double y, double z, double vx, double vy, double vz, SpriteSet spriteSet) {
+		super(world, x, y, z);
+		this.spriteSet = spriteSet;
+		this.setSize(0.2f, 0.2f);
+		this.lifetime = 6;
+		this.gravity = 0f;
+		this.hasPhysics = false;
+		this.xd = vx * 1;
+		this.yd = vy * 1;
+		this.zd = vz * 1;
+		this.setSpriteFromAge(spriteSet);
+	}
+
+	@Override
+	public int getLightColor(float partialTick) {
+		return 15728880;
+	}
+
+	@Override
+	public ParticleRenderType getRenderType() {
+		return ParticleRenderType.PARTICLE_SHEET_OPAQUE;
+	}
+
+	@Override
+	public void tick() {
+		super.tick();
+		if (!this.removed) {
+			this.setSprite(this.spriteSet.get((this.age / 1) % 6 + 1, 6));
+		}
+	}
+}
