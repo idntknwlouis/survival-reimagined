a3ffd74c06422b7f72557b20be7770f175ebcc8b
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 20:43:17 +0800

    Fix Bronze knife block tags

--- src/main/java/net/mcreator/survivalreimagined/item/BronzeKnifeItem.java
+++ src/main/java/net/mcreator/survivalreimagined/item/BronzeKnifeItem.java
@@ -1,7 +1,10 @@
 package net.mcreator.survivalreimagined.item;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
@@ -10,6 +13,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
 import java.util.stream.Stream;
@@ -21,7 +25,7 @@ public class BronzeKnifeItem extends Item {
 				.add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, -2, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).build()));
 	}
 	@Override public float getDestroySpeed(ItemStack stack, BlockState state) {
-		return Stream.of(BlockTags.create("c:carcasses"), BlockTags.create("c:smaller_carcasses"), BlockTags.create("c:carcass/equine")).anyMatch(state::is) ? 6f : 1;
+		return Stream.of(tag("c:carcasses"), tag("c:smaller_carcasses"), tag("c:carcass/equine")).anyMatch(state::is) ? 6f : 1;
 	}
 	@Override public boolean mineBlock(ItemStack stack, Level level, BlockState state, BlockPos pos, LivingEntity entity) {
 		stack.hurtAndBreak(1, entity, LivingEntity.getSlotForHand(entity.getUsedItemHand())); return true;
@@ -30,4 +34,8 @@ public class BronzeKnifeItem extends Item {
 		stack.hurtAndBreak(2, attacker, LivingEntity.getSlotForHand(attacker.getUsedItemHand())); return true;
 	}
 	@Override public int getEnchantmentValue() { return 14; }
+
+	private static TagKey<Block> tag(String id) {
+		return TagKey.create(Registries.BLOCK, ResourceLocation.parse(id));
+	}
 }
