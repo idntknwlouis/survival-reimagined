0f84e9c036c05a9ed89df2cbaa0c7be69a227fd1
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 18:11:41 +0800

    Fix obsolete tool item overrides

--- src/main/java/net/mcreator/survivalreimagined/item/FlintToolItem.java
+++ src/main/java/net/mcreator/survivalreimagined/item/FlintToolItem.java
@@ -39,23 +39,4 @@ public class FlintToolItem extends Item {
 		return 2;
 	}
 
-	@Override
-	public boolean hasCraftingRemainingItem(ItemStack stack) {
-		return true;
-	}
-
-	@Override
-	public ItemStack getCraftingRemainingItem(ItemStack itemstack) {
-		ItemStack retval = new ItemStack(this);
-		retval.setDamageValue(itemstack.getDamageValue() + 1);
-		if (retval.getDamageValue() >= retval.getMaxDamage()) {
-			return ItemStack.EMPTY;
-		}
-		return retval;
-	}
-
-	@Override
-	public boolean isRepairable(ItemStack itemstack) {
-		return false;
-	}
 }
