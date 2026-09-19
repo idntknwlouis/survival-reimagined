2a4a81aba8699725f1bb0315ea3da698db4b8202
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 15:00:42 +0800

    Port andesite surface rock block

---
+++ src/main/java/net/mcreator/survivalreimagined/block/AndesiteRockBlockBlock.java
@@ -0,0 +1,52 @@
+package net.mcreator.survivalreimagined.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+import net.mcreator.survivalreimagined.init.SurvivalReimaginedModItems;
+import net.mcreator.survivalreimagined.procedures.AndesiteRockBlockNeighbourBlockChangesProcedure;
+
+public class AndesiteRockBlockBlock extends Block {
+	private static final VoxelShape SHAPE = box(6, 0, 5, 10, 2, 11);
+
+	public AndesiteRockBlockBlock() {
+		super(BlockBehaviour.Properties.of()
+				.instabreak()
+				.noOcclusion()
+				.isRedstoneConductor((state, getter, pos) -> false)
+				.dynamicShape()
+				.offsetType(Block.OffsetType.XZ));
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		Vec3 offset = state.getOffset(world, pos);
+		return SHAPE.move(offset.x, offset.y, offset.z);
+	}
+
+	@Override
+	public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+		return Shapes.empty();
+	}
+
+	@Override
+	public ItemStack getCloneItemStack(LevelReader level, BlockPos pos, BlockState state) {
+		return new ItemStack(SurvivalReimaginedModItems.ANDESITE_ROCK.get());
+	}
+
+	@Override
+	public void neighborChanged(BlockState state, Level world, BlockPos pos, Block neighborBlock, BlockPos fromPos, boolean moving) {
+		super.neighborChanged(state, world, pos, neighborBlock, fromPos, moving);
+		AndesiteRockBlockNeighbourBlockChangesProcedure.execute(world, pos.getX(), pos.getY(), pos.getZ());
+	}
+}
