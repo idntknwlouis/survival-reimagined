349f68293efb74c421642b6bf2e23d17dac11822
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 14:13:48 +0800

    Port TinIngotItem.java to Fabric

---
+++ src/main/java/net/mcreator/survivalreimagined/item/TinIngotItem.java
@@ -0,0 +1,9 @@
+package net.mcreator.survivalreimagined.item;
+
+import net.minecraft.world.item.Item;
+
+public class TinIngotItem extends Item {
+	public TinIngotItem() {
+		super(new Item.Properties());
+	}
+}
