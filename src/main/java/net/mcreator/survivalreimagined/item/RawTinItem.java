89b101bb0ba3883ae729a3a3f6b5d633cc9bd389
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 14:13:45 +0800

    Port RawTinItem.java to Fabric

---
+++ src/main/java/net/mcreator/survivalreimagined/item/RawTinItem.java
@@ -0,0 +1,9 @@
+package net.mcreator.survivalreimagined.item;
+
+import net.minecraft.world.item.Item;
+
+public class RawTinItem extends Item {
+	public RawTinItem() {
+		super(new Item.Properties());
+	}
+}
