31d315aeb4a3aeeb6ef3b86c350c0c96786527f9
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 14:13:52 +0800

    Port TinChunkItem.java to Fabric

---
+++ src/main/java/net/mcreator/survivalreimagined/item/TinChunkItem.java
@@ -0,0 +1,9 @@
+package net.mcreator.survivalreimagined.item;
+
+import net.minecraft.world.item.Item;
+
+public class TinChunkItem extends Item {
+	public TinChunkItem() {
+		super(new Item.Properties());
+	}
+}
