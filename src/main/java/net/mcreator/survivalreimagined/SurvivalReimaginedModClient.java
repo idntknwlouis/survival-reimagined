ce79145a31b098f0710dbc6fa5bafd5dcae4a225
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 13:45:59 +0800

    Add Fabric client initializer

---
+++ src/main/java/net/mcreator/survivalreimagined/SurvivalReimaginedModClient.java
@@ -0,0 +1,10 @@
+package net.mcreator.survivalreimagined;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SurvivalReimaginedModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// Client renderers, screens, item properties and particles are ported here incrementally.
+	}
+}
