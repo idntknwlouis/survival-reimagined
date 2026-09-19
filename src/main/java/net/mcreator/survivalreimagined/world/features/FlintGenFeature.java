801ecdb76d898910aad2a1794a2bccbc2f88e0c7
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-19 14:38:55 +0800

    Port custom Fabric feature types

---
+++ src/main/java/net/mcreator/survivalreimagined/world/features/FlintGenFeature.java
@@ -0,0 +1,23 @@
+package net.mcreator.survivalreimagined.world.features;
+
+import net.minecraft.world.level.WorldGenLevel;
+import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.levelgen.feature.RandomPatchFeature;
+import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
+
+import net.mcreator.survivalreimagined.procedures.FlintGenAdditionalGenerationConditionProcedure;
+
+public class FlintGenFeature extends RandomPatchFeature {
+	public FlintGenFeature() {
+		super(RandomPatchConfiguration.CODEC);
+	}
+
+	@Override
+	public boolean place(FeaturePlaceContext<RandomPatchConfiguration> context) {
+		WorldGenLevel world = context.level();
+		int x = context.origin().getX();
+		int y = context.origin().getY();
+		int z = context.origin().getZ();
+		return FlintGenAdditionalGenerationConditionProcedure.execute(world, x, y, z) && super.place(context);
+	}
+}
