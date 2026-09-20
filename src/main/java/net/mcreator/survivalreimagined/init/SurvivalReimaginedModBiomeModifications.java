547cebeef438e163a14a971a8f15f71a7e6721de
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-20 14:43:09 +0800

    Enable remaining basalt ore worldgen

--- src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModBiomeModifications.java
+++ src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModBiomeModifications.java
@@ -20,6 +20,14 @@ public final class SurvivalReimaginedModBiomeModifications {
 		addUndergroundEverywhere("diamond_ore_feature");
 		addUndergroundEverywhere("emerald_ore_feature");
 		addUndergroundEverywhere("lazurite_ore");
+		addUndergroundEverywhere("basalt_hematite_feature");
+		addUndergroundEverywhere("basalt_magnetite_feature");
+		addUndergroundEverywhere("basalt_calaverite_feature");
+		addUndergroundEverywhere("basalt_pyrolusite_feature");
+		addUndergroundEverywhere("basalt_uranophane_feature");
+		addUndergroundEverywhere("basalt_ilmenite_feature");
+		addUndergroundEverywhere("basalt_anthracite_feature");
+		addUndergroundEverywhere("basalt_liginite_feature");
 		addUndergroundEverywhere("uraninite_feature");
 		addUndergroundEverywhere("titanium_ore_feature");
 		addUndergroundEverywhere("amber_ore_feature");
