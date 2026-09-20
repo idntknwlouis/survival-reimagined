9cbb655999e0689a2ec7cf3c908f315d6e06fb2a
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-20 12:59:22 +0800

    Register polished shale items

--- src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModItems.java
+++ src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModItems.java
@@ -51,6 +51,9 @@ public final class SurvivalReimaginedModItems {
 	public static final RegistryEntry<Item> OBSIDIAN_ROCK = register("obsidian_rock", () -> new SurfaceRockItem(SurvivalReimaginedModBlocks.OBSIDIAN_ROCK::get, "block.stone.place"));
 	public static final RegistryEntry<Item> SHALE = block(SurvivalReimaginedModBlocks.SHALE);
 	public static final RegistryEntry<Item> SHALE_ROCK = block(SurvivalReimaginedModBlocks.SHALE_ROCK);
+	public static final RegistryEntry<Item> POLISHED_SHALE = block(SurvivalReimaginedModBlocks.POLISHED_SHALE);
+	public static final RegistryEntry<Item> POLISHED_SHALE_BRICKS = block(SurvivalReimaginedModBlocks.POLISHED_SHALE_BRICKS);
+	public static final RegistryEntry<Item> POLISHED_CHISELED_SHALE = block(SurvivalReimaginedModBlocks.POLISHED_CHISELED_SHALE);
 	public static final RegistryEntry<Item> OBSIDIAN_HANDLE = simple("obsidian_handle");
 	public static final RegistryEntry<Item> SMALL_OBSIDIAN_HANDLE = simple("small_obsidian_handle");
 	public static final RegistryEntry<Item> FLINT_TOOL = register("flint_tool", FlintToolItem::new);
