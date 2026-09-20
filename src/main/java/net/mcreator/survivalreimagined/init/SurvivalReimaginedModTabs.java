34efc1f2a7121abd9f99f802ca42783ff4ffde1f
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-20 18:32:48 +0800

    Add Uranium Rod to creative tab

--- src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModTabs.java
+++ src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModTabs.java
@@ -274,6 +274,7 @@ public final class SurvivalReimaginedModTabs {
 					output.accept(SurvivalReimaginedModItems.EMPTY_GOLD_RUNE.get());
 					output.accept(SurvivalReimaginedModItems.RUNE_MAGIC_INFUSER.get());
 					output.accept(SurvivalReimaginedModItems.ADVANCED_ALLOY_FORGE.get());
+					output.accept(SurvivalReimaginedModItems.URANIUM_ROD.get());
 					output.accept(SurvivalReimaginedModItems.EMPTY_SILVER_RUNE.get());
 					output.accept(SurvivalReimaginedModItems.SAPPHIRE.get());
 					output.accept(SurvivalReimaginedModItems.AMBER.get());
