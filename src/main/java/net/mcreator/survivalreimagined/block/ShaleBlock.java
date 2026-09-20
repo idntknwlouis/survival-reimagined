d80c72d31dcf361929b5f2e9bcb541acb56f034d
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-20 12:46:58 +0800

    Add core shale blocks

---
+++ src/main/java/net/mcreator/survivalreimagined/block/ShaleBlock.java
@@ -0,0 +1,38 @@
+package net.mcreator.survivalreimagined.block;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraft.world.level.block.Rotation;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.EnumProperty;
+
+public class ShaleBlock extends Block {
+	public static final EnumProperty<Direction.Axis> AXIS = BlockStateProperties.AXIS;
+
+	public ShaleBlock() {
+		super(BlockBehaviour.Properties.of().strength(1.5F, 6.0F).requiresCorrectToolForDrops());
+		this.registerDefaultState(this.stateDefinition.any().setValue(AXIS, Direction.Axis.Y));
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+		super.createBlockStateDefinition(builder);
+		builder.add(AXIS);
+	}
+
+	@Override
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		BlockState state = super.getStateForPlacement(context);
+		return state == null ? null : state.setValue(AXIS, context.getClickedFace().getAxis());
+	}
+
+	@Override
+	protected BlockState rotate(BlockState state, Rotation rotation) {
+		return RotatedPillarBlock.rotatePillar(state, rotation);
+	}
+}
