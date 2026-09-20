fe6349bf80a8cb8abce4405a268eb2cb27cb684e
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-20 20:01:09 +0800

    Add functional scripture menus

---
+++ src/main/java/net/mcreator/survivalreimagined/world/inventory/AAFScriptureGUIMenu.java
@@ -0,0 +1,30 @@
+package net.mcreator.survivalreimagined.world.inventory;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+import net.minecraft.world.item.ItemStack;
+
+import net.mcreator.survivalreimagined.init.SurvivalReimaginedModMenus;
+
+public class AAFScriptureGUIMenu extends AbstractContainerMenu {
+	private final BlockPos pos;
+
+	public AAFScriptureGUIMenu(int id, Inventory inventory, BlockPos pos) {
+		super(SurvivalReimaginedModMenus.AAF_SCRIPTURE_GUI.get(), id);
+		this.pos = pos;
+	}
+
+	public BlockPos getPos() { return pos; }
+
+	@Override
+	public ItemStack quickMoveStack(Player player, int index) {
+		return ItemStack.EMPTY;
+	}
+
+	@Override
+	public boolean stillValid(Player player) {
+		return true;
+	}
+}
