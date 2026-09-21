866eaef5f2c5669dad1d7a69d4032e46e6e2046d
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-21 22:57:01 +0800

    Register millstone screen

--- src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModScreens.java
+++ src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModScreens.java
@@ -6,6 +6,7 @@ import net.mcreator.survivalreimagined.client.gui.ForgeGUIScreen;
 import net.mcreator.survivalreimagined.client.gui.AdvancedAlloyForgeGUIScreen;
 import net.mcreator.survivalreimagined.client.gui.MetalRefiningTableGUIScreen;
 import net.mcreator.survivalreimagined.client.gui.MPTGUIScreen;
+import net.mcreator.survivalreimagined.client.gui.MillstoneGUIScreen;
 import net.mcreator.survivalreimagined.client.gui.RMIScreen;
 
 public final class SurvivalReimaginedModScreens {
@@ -16,6 +17,7 @@ public final class SurvivalReimaginedModScreens {
 		MenuScreens.register(SurvivalReimaginedModMenus.FORGE_GUI.get(), ForgeGUIScreen::new);
 		MenuScreens.register(SurvivalReimaginedModMenus.METAL_REFINING_TABLE_GUI.get(), MetalRefiningTableGUIScreen::new);
 		MenuScreens.register(SurvivalReimaginedModMenus.MPTGUI.get(), MPTGUIScreen::new);
+		MenuScreens.register(SurvivalReimaginedModMenus.MILLSTONE_GUI.get(), MillstoneGUIScreen::new);
 		MenuScreens.register(SurvivalReimaginedModMenus.RMI.get(), RMIScreen::new);
 		MenuScreens.register(SurvivalReimaginedModMenus.ADVANCED_ALLOY_FORGE_GUI.get(), AdvancedAlloyForgeGUIScreen::new);
 	}
