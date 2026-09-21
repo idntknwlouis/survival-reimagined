232319c0ac758480980175c506e8c2cb254c62d8
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-21 12:38:57 +0800

    Use native button for Rune Magic Infuser

--- src/main/java/net/mcreator/survivalreimagined/client/gui/RMIScreen.java
+++ src/main/java/net/mcreator/survivalreimagined/client/gui/RMIScreen.java
@@ -1,8 +1,7 @@
 package net.mcreator.survivalreimagined.client.gui;
 
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.ImageButton;
-import net.minecraft.client.gui.components.WidgetSprites;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
@@ -14,11 +13,8 @@ public class RMIScreen extends AbstractContainerScreen<RMIMenu> {
 	private static final ResourceLocation BACKGROUND = ResourceLocation.fromNamespaceAndPath("survival_reimagined", "textures/screens/rmi.png");
 	private static final ResourceLocation RUNE_OUTLINE = ResourceLocation.fromNamespaceAndPath("survival_reimagined", "textures/screens/rune_putline.png");
 	private static final ResourceLocation LAPIS_OUTLINE = ResourceLocation.fromNamespaceAndPath("survival_reimagined", "textures/screens/lapis_outline.png");
-	private static final ResourceLocation BUTTON = ResourceLocation.fromNamespaceAndPath("survival_reimagined", "textures/screens/rmi_button.png");
-	private static final ResourceLocation BUTTON_HOVERED = ResourceLocation.fromNamespaceAndPath("survival_reimagined", "textures/screens/rmi_hovered.png");
-	private static final ResourceLocation BUTTON_DISABLED = ResourceLocation.fromNamespaceAndPath("survival_reimagined", "textures/screens/rmi_unpressable.png");
 
-	private ImageButton infuseButton;
+	private Button infuseButton;
 
 	public RMIScreen(RMIMenu menu, Inventory inventory, Component title) {
 		super(menu, inventory, title);
@@ -29,16 +25,12 @@ public class RMIScreen extends AbstractContainerScreen<RMIMenu> {
 	@Override
 	protected void init() {
 		super.init();
-		this.infuseButton = new ImageButton(
-				this.leftPos + 72, this.topPos + 61, 32, 16,
-				new WidgetSprites(BUTTON, BUTTON_HOVERED),
-				button -> {
-					if (this.minecraft != null && this.minecraft.gameMode != null && this.minecraft.player != null
-							&& this.menu.canInfuse(this.minecraft.player)) {
-						this.minecraft.gameMode.handleInventoryButtonClick(this.menu.containerId, 0);
-					}
-				}
-		);
+		this.infuseButton = Button.builder(Component.literal("Infuse"), button -> {
+			if (this.minecraft != null && this.minecraft.gameMode != null && this.minecraft.player != null
+					&& this.menu.canInfuse(this.minecraft.player)) {
+				this.minecraft.gameMode.handleInventoryButtonClick(this.menu.containerId, 0);
+			}
+		}).bounds(this.leftPos + 72, this.topPos + 61, 32, 16).build();
 		this.addRenderableWidget(this.infuseButton);
 	}
 
@@ -55,10 +47,6 @@ public class RMIScreen extends AbstractContainerScreen<RMIMenu> {
 		graphics.blit(BACKGROUND, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight, this.imageWidth, this.imageHeight);
 		graphics.blit(RUNE_OUTLINE, this.leftPos + 80, this.topPos + 41, 0, 0, 16, 16, 16, 16);
 		graphics.blit(LAPIS_OUTLINE, this.leftPos + 116, this.topPos + 61, 0, 0, 16, 16, 16, 16);
-
-		if (this.minecraft != null && this.minecraft.player != null && !this.menu.canInfuse(this.minecraft.player)) {
-			graphics.blit(BUTTON_DISABLED, this.leftPos + 72, this.topPos + 61, 0, 0, 32, 16, 32, 16);
-		}
 	}
 
 	@Override
