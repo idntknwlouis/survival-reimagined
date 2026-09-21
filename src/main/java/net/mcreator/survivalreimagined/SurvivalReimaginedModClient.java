1a28e7510a924b27a7184977a90574b58ad5b90d
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-21 13:31:44 +0800

    Render Uranium Rod as translucent

--- src/main/java/net/mcreator/survivalreimagined/SurvivalReimaginedModClient.java
+++ src/main/java/net/mcreator/survivalreimagined/SurvivalReimaginedModClient.java
@@ -2,8 +2,11 @@ package net.mcreator.survivalreimagined;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
 
 import net.mcreator.survivalreimagined.init.SurvivalReimaginedModParticles;
+import net.mcreator.survivalreimagined.init.SurvivalReimaginedModBlocks;
 import net.mcreator.survivalreimagined.init.SurvivalReimaginedModScreens;
 import net.mcreator.survivalreimagined.util.RuneInfusionTooltip;
 
@@ -12,6 +15,7 @@ public class SurvivalReimaginedModClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		SurvivalReimaginedModParticles.register();
 		SurvivalReimaginedModScreens.register();
+		BlockRenderLayerMap.INSTANCE.putBlock(SurvivalReimaginedModBlocks.URANIUM_ROD.get(), RenderType.translucent());
 		ItemTooltipCallback.EVENT.register((stack, context, type, lines) -> RuneInfusionTooltip.append(stack, lines));
 	}
 }
