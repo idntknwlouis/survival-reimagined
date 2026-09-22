75e70abf755215e7e273af50b9db563ede154c4e
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-23 01:06:25 +0800

    Register Sheep carcass blocks

--- src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModBlocks.java
+++ src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModBlocks.java
@@ -54,6 +54,9 @@ public final class SurvivalReimaginedModBlocks {
 	public static final RegistryEntry<Block> PIG_CARCASS = register("pig_carcass", () -> new CarcassBlock(CarcassBlock.Species.PIG));
 	public static final RegistryEntry<Block> PIG_HEAD = register("pig_head", () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.MUD).strength(1.0F).noOcclusion()));
 	public static final RegistryEntry<Block> PIG_LEG = register("pig_leg", () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.MUD).strength(1.0F).noOcclusion()));
+	public static final RegistryEntry<Block> SHEEP_CARCASS = register("sheep_carcass", () -> new CarcassBlock(CarcassBlock.Species.SHEEP));
+	public static final RegistryEntry<Block> SHEEP_HEAD = register("sheep_head", () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.MUD).strength(1.0F).noOcclusion()));
+	public static final RegistryEntry<Block> SHEEP_LEG = register("sheep_leg", () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.MUD).strength(1.0F).noOcclusion()));
 	public static final RegistryEntry<Block> STONE_ROCK_BLOC = register("stone_rock_bloc", StoneRockBlocBlock::new);
 	public static final RegistryEntry<Block> OBSIDIAN_ROCK = register("obsidian_rock", () -> new SurfaceRockBlock(() -> SurvivalReimaginedModItems.OBSIDIAN_ROCK.get()));
 	public static final RegistryEntry<Block> SHALE = register("shale", ShaleBlock::new);
