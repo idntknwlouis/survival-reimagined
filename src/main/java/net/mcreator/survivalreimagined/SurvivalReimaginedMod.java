657853f8921904bba00df8141bc1fc69f947ff28
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-22 23:53:32 +0800

    Register Crimson Thread drops

--- src/main/java/net/mcreator/survivalreimagined/SurvivalReimaginedMod.java
+++ src/main/java/net/mcreator/survivalreimagined/SurvivalReimaginedMod.java
@@ -19,6 +19,7 @@ import net.minecraft.server.MinecraftServer;
 import net.mcreator.survivalreimagined.util.RuneEffects;
 import net.mcreator.survivalreimagined.util.CropEffects;
 import net.mcreator.survivalreimagined.util.FoodSpoilage;
+import net.mcreator.survivalreimagined.util.CrimsonThreadDrops;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -60,6 +61,7 @@ public class SurvivalReimaginedMod implements ModInitializer {
 		SurvivalReimaginedModBiomeModifications.register();
 		RuneEffects.register();
 		CropEffects.register();
+		CrimsonThreadDrops.register();
 
 		UseBlockCallback.EVENT.register((player, level, hand, hit) -> {
 			if (hand == net.minecraft.world.InteractionHand.MAIN_HAND) {
