8013652c87f05515f962e4b5f53dfe4fbadb50ab
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-22 17:27:57 +0800

    Make thin vine base Pick Block return normal vine

--- src/main/java/net/mcreator/survivalreimagined/block/ThinRadiatedVineBaseBlock.java
+++ src/main/java/net/mcreator/survivalreimagined/block/ThinRadiatedVineBaseBlock.java
@@ -2,6 +2,7 @@ package net.mcreator.survivalreimagined.block;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
@@ -22,6 +23,11 @@ public class ThinRadiatedVineBaseBlock extends Block {
     public ThinRadiatedVineBaseBlock() {
         super(BlockBehaviour.Properties.of().sound(SoundType.MOSS).strength(1f).noCollission()
                 .isRedstoneConductor((state, level, pos) -> false));
+    }
+
+    @Override
+    public ItemStack getCloneItemStack(LevelReader level, BlockPos pos, BlockState state) {
+        return new ItemStack(SurvivalReimaginedModBlocks.THIN_RADIATED_VINES.get().asItem());
     }
 
     @Override
