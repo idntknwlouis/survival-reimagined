5ce376451609171b36e932178c020616f261537d
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-23 00:10:21 +0800

    Register cow carcass blocks

--- src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModBlocks.java
+++ src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModBlocks.java
@@ -48,6 +48,9 @@ import java.util.function.Supplier;
 
 public final class SurvivalReimaginedModBlocks {
 	public static final RegistryEntry<Block> FLINTBLOCK = register("flintblock", FlintblockBlock::new);
+	public static final RegistryEntry<Block> COW_CARCASS = register("cow_carcass", () -> new CarcassBlock(CarcassBlock.Species.COW));
+	public static final RegistryEntry<Block> COW_HEAD = register("cow_head", () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.MUD).strength(1.0F).noOcclusion()));
+	public static final RegistryEntry<Block> COW_LEG = register("cow_leg", () -> new Block(BlockBehaviour.Properties.of().sound(SoundType.MUD).strength(1.0F).noOcclusion()));
 	public static final RegistryEntry<Block> STONE_ROCK_BLOC = register("stone_rock_bloc", StoneRockBlocBlock::new);
 	public static final RegistryEntry<Block> OBSIDIAN_ROCK = register("obsidian_rock", () -> new SurfaceRockBlock(() -> SurvivalReimaginedModItems.OBSIDIAN_ROCK.get()));
 	public static final RegistryEntry<Block> SHALE = register("shale", ShaleBlock::new);
