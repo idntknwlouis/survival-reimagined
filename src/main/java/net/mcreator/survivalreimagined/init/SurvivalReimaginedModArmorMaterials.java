4157fb3b4b57239bdb0b50d78c936d1357f751d6
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-22 20:54:53 +0800

    Restore armor material registry import

--- src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModArmorMaterials.java
+++ src/main/java/net/mcreator/survivalreimagined/init/SurvivalReimaginedModArmorMaterials.java
@@ -3,6 +3,7 @@ package net.mcreator.survivalreimagined.init;
 import net.minecraft.Util;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.ArmorItem;
