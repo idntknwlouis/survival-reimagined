ac770aca9e12f691efd1c6bac899bdc9f5b8b888
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-22 22:59:08 +0800

    Restore boar family custom renderers

--- src/main/java/net/mcreator/survivalreimagined/client/renderer/PigletRenderer.java
+++ src/main/java/net/mcreator/survivalreimagined/client/renderer/PigletRenderer.java
@@ -1,29 +1,22 @@
 package net.mcreator.survivalreimagined.client.renderer;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.model.PigModel;
-import net.minecraft.client.model.geom.ModelLayers;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.resources.ResourceLocation;
 
+import net.mcreator.survivalreimagined.client.model.Modelpiglet;
 import net.mcreator.survivalreimagined.entity.PigletEntity;
 
-public class PigletRenderer extends MobRenderer<PigletEntity, PigModel<PigletEntity>> {
-	private static final ResourceLocation TEMP_TEXTURE =
-			ResourceLocation.fromNamespaceAndPath("minecraft", "textures/entity/pig/pig.png");
+public class PigletRenderer extends MobRenderer<PigletEntity, Modelpiglet<PigletEntity>> {
+	private static final ResourceLocation TEXTURE =
+			ResourceLocation.fromNamespaceAndPath("survival_reimagined", "textures/entities/piglet.png");
 
 	public PigletRenderer(EntityRendererProvider.Context context) {
-		super(context, new PigModel<>(context.bakeLayer(ModelLayers.PIG)), 0.25F);
-	}
-
-	@Override
-	protected void scale(PigletEntity entity, PoseStack poseStack, float partialTickTime) {
-		poseStack.scale(0.5F, 0.5F, 0.5F);
+		super(context, new Modelpiglet<>(context.bakeLayer(Modelpiglet.LAYER_LOCATION)), 0.25F);
 	}
 
 	@Override
 	public ResourceLocation getTextureLocation(PigletEntity entity) {
-		return TEMP_TEXTURE;
+		return TEXTURE;
 	}
 }
