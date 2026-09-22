4761778b4776a6f8095c52449ed1509ceedb0622
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-22 22:40:15 +0800

    Import Mob for sow attributes

--- src/main/java/net/mcreator/survivalreimagined/entity/SowEntity.java
+++ src/main/java/net/mcreator/survivalreimagined/entity/SowEntity.java
@@ -6,6 +6,7 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
