b6f7d7ea94c85afa0c48eaff9b234c61730a346d
louis :) <107615094+idntknwlouis@users.noreply.github.com>
2026-09-24 02:03:07 +0800

    Add Tropical Coast sand surface rules

--- src/main/java/net/mcreator/survivalreimagined/world/worldgen/SurvivalReimaginedModSurfaceRules.java
+++ src/main/java/net/mcreator/survivalreimagined/world/worldgen/SurvivalReimaginedModSurfaceRules.java
@@ -43,6 +43,22 @@ public class SurvivalReimaginedModSurfaceRules {
                         )
                 ),
                 SurfaceRules.ifTrue(
+                        SurfaceRules.isBiome(SurvivalReimaginedModBiomes.TROPICAL_COAST),
+                        SurfaceRules.ifTrue(
+                                SurfaceRules.abovePreliminarySurface(),
+                                SurfaceRules.sequence(
+                                        SurfaceRules.ifTrue(
+                                                SurfaceRules.ON_FLOOR,
+                                                SurfaceRules.state(Blocks.SAND.defaultBlockState())
+                                        ),
+                                        SurfaceRules.ifTrue(
+                                                SurfaceRules.UNDER_FLOOR,
+                                                SurfaceRules.state(Blocks.SAND.defaultBlockState())
+                                        )
+                                )
+                        )
+                ),
+                SurfaceRules.ifTrue(
                         SurfaceRules.isBiome(SurvivalReimaginedModBiomes.WISTERIA_FOREST),
                         SurfaceRules.ifTrue(
                                 SurfaceRules.abovePreliminarySurface(),
